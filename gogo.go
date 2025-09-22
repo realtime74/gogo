a4838ff09cfbfe5dc6e24dfe728d3169f744b38e
realtime74 <realtime74@proton.me>
2025-09-22 21:57:34 +0000

    add +/~/- functions.

--- gogo.go
+++ gogo.go
@@ -8,6 +8,8 @@ import (
 )
 
 type GogoFile struct {
+	Home string
+
 	file *os.File
 }
 
@@ -16,12 +18,24 @@ func (g *GogoFile) Init() string {
 	if err != nil {
 		panic(err)
 	}
+	g.Home = hd
 	fp := hd + "/.gogo"
 	g.file, err = os.OpenFile(fp, os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
 		panic(err)
 	}
-	return "Hello, Gogo!"
+}
+
+func (g *GogoFile) Append(dir string) {
+	g.file.Seek(0, 2) // seek to end of file
+	info, err := g.file.Stat()
+	if err != nil {
+		panic(err)
+	}
+	if info.Size() > 0 {
+		g.file.WriteString("\n")
+	}
+	g.file.WriteString(dir)
 }
 
 func (g *GogoFile) Match(pattern string) string {
@@ -58,10 +72,23 @@ func main() {
 
 	// check first command line argument
 	if len(os.Args) < 2 {
-		fmt.Println("usage: gogo <pattern|+>")
-		// bail out
-		os.Exit(1)
+		fmt.Println(file.Home)
+		return
 	}
 	pattern := os.Args[1]
-	fmt.Println(file.Match(pattern))
+	switch pattern {
+	case "~":
+		fmt.Println(file.Home)
+		return
+	case "-":
+		fmt.Println("-")
+		return
+	case "+":
+		cwd, _ := os.Getwd()
+		file.Append(cwd)
+		fmt.Println(cwd)
+		return
+	}
+	dir := file.Match(pattern)
+	fmt.Println(dir)
 }
